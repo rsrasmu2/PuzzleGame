17d0f89cceef29d4aecf3f5e97e5fd7dbed02353
Nancy <nm389@nau.edu>
2015-02-11 18:10:17 -0700

    Revert "added ship animation to the load screen"
    This reverts commit 65bbb3950a38a90795cb66bb607f7cb1b3ea0626.

--- src/Startup.hx
+++ src/Startup.hx
@@ -1,23 +1,15 @@
 import flash.display.Bitmap;
-import flash.display.BitmapData;
 import flash.display.Sprite;
 import flash.events.Event;
 import flash.geom.Rectangle;
 import flash.Lib;
 import starling.core.Starling;
-import starling.animation.Tween;
-import starling.animation.Transitions;
 
 @:bitmap("assets/loading.png")
 class LoadingBitmapData extends flash.display.BitmapData { }
 
-@:bitmap("assets/crew.png")
-class CrewBitmapData extends flash.display.BitmapData { }
-
 class Startup extends Sprite {
 	public var loadingBitmap:Bitmap;
-	public var crewBitmap:Bitmap;
-	
 	public var startup:Sprite;
 
 	function new() {
@@ -32,12 +24,6 @@ class Startup extends Sprite {
 		loadingBitmap.smoothing = true;
 		addChild(loadingBitmap);	//To display on scene
 
-		crewBitmap = new Bitmap(new CrewBitmapData(300, 112));
-		crewBitmap.x = (Lib.current.stage.stageWidth/2) - 150;
-		crewBitmap.y = (Lib.current.stage.stageWidth/2) - 56;
-		addChild(crewBitmap);
-		//up(crewBitmap);
-		
 		Lib.current.stage.addEventListener(Event.RESIZE, function(e:Event) {
 			Starling.current.viewPort = new Rectangle(0, 0, Lib.current.stage.stageWidth, Lib.current.stage.stageHeight);
 			if (loadingBitmap != null) {
@@ -57,17 +43,7 @@ class Startup extends Sprite {
 
 		mStarling.addEventListener(starling.events.Event.ROOT_CREATED, onRootCreated);
 	}
-		/*
-		public function down(){
-			Starling.juggler.tween(crewBitmap, 3, { transition: Transitions.LINEAR,
-			y: crewBitmap.y-15,
-			onComplete: function() {
-			up();	
-			}
-		});
-		}
-		*/
-	
+
 	static function main() {
 		var stage = Lib.current.stage;
 		stage.addChild(new Startup());
